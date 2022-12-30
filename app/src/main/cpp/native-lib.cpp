5a5bc171c92686c493341bf37adcb89f6cbdf2a2
zhangqingfa <1583510917@qq.com>
2022-12-30 09:26:16 +0800

    修改nagive-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_zqf_vagrantiptv_ui_main_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+Java_com_zqf_vagrantiptv_ui_main_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
