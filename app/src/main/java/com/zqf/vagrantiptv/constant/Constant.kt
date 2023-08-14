883e076f61e98e8b071bc7ea1e12835470f5c5a8
码农砖家 <1583510917@qq.com>
2023-08-14 17:10:33 +0800

    Update Constant.kt
    test url

--- app/src/main/java/com/zqf/vagrantiptv/constant/Constant.kt
+++ app/src/main/java/com/zqf/vagrantiptv/constant/Constant.kt
@@ -5,5 +5,6 @@ public class Constant {
     companion object {
         const val delayTime = 2000L
         const val key = "position"
+        const val testurl = "https://mbd.baidu.com/newspage/data/videolanding?nid=sv_9190112807815022780&sourceFrom=pc_feedlist"
     }
 }
